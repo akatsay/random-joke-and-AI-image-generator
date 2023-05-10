daaac272d100c6f82e957b6127d4601b6fc4de3a
a_katsay a_katsay <a_katsay@mail.ru>
2023-05-10 09:46:02 -0400

    config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ module.exports = withExpo({
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: [
+    "@expo/webpack-config",
+    "@react-navigation/native",
+    "@react-navigation/native-stack",
+    "expo-status-bar",
+    "react-native-safe-area-context",
+    "react-native-screens",
+    "react-native-url-polyfill",
     // Expo/React Native packages need to be transpiled here
     'react-native',
     'react-native-web',
